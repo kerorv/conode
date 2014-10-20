564f7533db84b49a6806b06e6209217c22c8bc08
kerorv <jianxihua@gmail.com>
2014-10-20 12:54:31 +0800

    完善mainnode.lua

--- mainnode.lua
+++ mainnode.lua
@@ -1,24 +1,11 @@
 -- mainnode.lua
 
-require "msgtype"
+local conode = require "conode"
 local sproto = require "sproto"
+local msgtypes = require "msgtype"
 
 MainNode = {}
 MainNode.__index = MainNode
-
-local function loadpb(...)
-	local pbs = {}
-	for i, v in ipairs{...} do
-		print(v)
-		local file = io.open(v, "rb")
-		assert(file)
-		pb = file:read("*a")
-		file:close()
-		print("pblen=" .. #pb)
-		pbs[i] = pb
-	end
-	return table.concat(pbs)
-end
 
 local function loadproto(...)
 	local protos = {}
@@ -31,16 +18,36 @@ local function loadproto(...)
 	return table.concat(protos, "\n")
 end
 
+local function sendclientmsg(node, sid, type, msg)
+	local clientmsg = { sid = sid, msgtype = type, msg = msg }
+	local spmsg = node.sp:encode("SocketSendClientMsg", clientmsg)
+	conode.sendmsg(100--[[socketnode id]], msgtypes.SOCKET_SENDCLIENTMSG, spmsg)
+end
+
+local function onclientmsg(node, sid, msgtype, msg)
+	if msgtype == msgtypes.PLAYER_LOGIN then
+		local loginmsg = node.sp:decode("LoginMsg", msg)
+		assert(loginmsg)
+		print("LoginMsg from sid=" .. sid .. 
+				" username=" .. loginmsg.username ..
+				" password=" .. loginmsg.password)
+	elseif msgtype == msgtypes.PLAYER_ECHO then
+		local echomsg = node.sp:decode("EchoMsg", msg)
+		assert(echomsg)
+		print("EchoMsg from sid=" .. sid .. " msg=" .. echomsg.msg)
+		local response = { msg = echomsg.msg }
+		local respmsg = node.sp:encode("EchoMsg", response)
+		sendclientmsg(node, sid, msgtypes.PLAYER_ECHO, respmsg)
+	end
+end
+
 function MainNode.New(nid)
 	local node = {}
 	setmetatable(node, MainNode)
 	node.id = nid
 	print("MainNode.New " .. nid)
 
---	local pb = loadpb("proto/socketnode.pb", "proto/client.pb")
---	print("pb total len=" .. #pb)
 	local proto = loadproto("proto/socketnode.sp", "proto/client.sp")
-	print(proto)
 	node.sp = sproto.parse(proto)
 	assert(node.sp)
 	return node
@@ -50,44 +57,24 @@ function MainNode:Release()
 	print("MainNode:Release()")
 end
 
-function MainNode:SendClientMsg(sid, msgtype, msg)
-	local clientmsg = { sid = sid, msgtype = msgtype, msg = msg }
-	local spmsg = self.sp:encode("SocketSendClientMsg", clientmsg)
-	sendmsg(100--[[socketnode id]], MSG_TYPE_SOCKET_SENDCLIENTMSG, spmsg)
-end
-
-function MainNode:OnClientMsg(sid, msgtype, msg)
-	if msgtype == MSG_TYPE_PLAYER_LOGIN then
-		local loginmsg = self.sp:decode("LoginMsg", msg)
-		assert(loginmsg)
-		print("LoginMsg from sid=" .. sid .. 
-				" username=" .. loginmsg.username ..
-				" password=" .. loginmsg.password)
-	elseif msgtype == MSG_TYPE_ECHO then
-		local echomsg = self.sp:decode("EchoMsg", msg)
-		assert(echomsg)
-		print("EchoMsg from sid=" .. sid .. " msg=" .. echomsg.msg)
-		local response = { msg = echomsg.msg }
-		local respmsg = self.sp:encode("EchoMsg", response)
-		self:SendClientMsg(sid, MSG_TYPE_ECHO, respmsg)
-	end
-end
-
 function MainNode:OnMessage(type, ptr, len)
-	if type == MSG_TYPE_SOCKET_RECVCLIENTMSG then
+	if type == msgtypes.SOCKET_RECVCLIENTMSG then
 		local clientmsg = self.sp:decodeud("SocketRecvClientMsg", ptr, len)
+		print(clientmsg)
 		assert(clientmsg)
-		self:OnClientMsg(clientmsg.sid, clientmsg.msgtype, clientmsg.msg)
-	elseif type == MSG_TYPE_SOCKET_CONNECTIONBREAK then
+		onclientmsg(self, clientmsg.sid, clientmsg.msgtype, clientmsg.msg)
+	elseif type == msgtypes.SOCKET_CONNECTIONBREAK then
 		local connbreakmsg = self.sp:decodeud(
 				"SocketConnectionBreakMsg",
 				ptr, 
 				len)	
 		assert(connbreakmsg)
-		print("Connection[" .. connbreakmsg.sid .. "] is broken. reason:" .. connbreakmsg.reason)
+		print("Connection[" .. connbreakmsg.sid 
+				.. "] is broken. reason:" 
+				.. connbreakmsg.reason)
 		local rmconnmsg = { sid = connbreakmsg.sid }
 		local spmsg = self.sp:encode("SocketRemoveConnectionMsg", rmconnmsg)
-		sendmsg(100--[[socketnode id]], MSG_TYPE_SOCKET_REMOVECONNECTION, spmsg)
+		conode.sendmsg(100--[[socketnode id]], msgtypes.SOCKET_REMOVECONNECTION, spmsg)
 	end
 end
 
